a4fd324c616a3e1a010fc2ba4fb97b7ae374dd42
meet3475 <meetdobariya480@gmail.com>
2024-08-23 10:11:32 +0530

    cs

--- src/middlewar/upload.js
+++ src/middlewar/upload.js
@@ -4,27 +4,30 @@ const path = require("path");
 
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      // const FilePath = path.join("public", file.fieldname);
-      // console.log(FilePath);
-      const fieldname = file.fieldname;
-      const FilePath = path.join("public/temp", fieldname);
+  destination: function (req, file, cb) {
+    // const FilePath = path.join("public", file.fieldname);
+    // console.log(FilePath);
+    const fieldname = file.fieldname;
+    // const FilePath = path.join("public/temp", fieldname);
 
-      console.log(FilePath, fieldname);
-      
-      fs.mkdir(FilePath, { recursive: true }, (err) => {
-        if (err) {
-           return cb(err, null)
-        }
-      });
-      cb(null, FilePath)
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, uniqueSuffix + '-' + file.originalname)
-    }
-  })
-  
-  const upload = multer({ storage: storage });
 
-  module.exports = upload;
+    const FilePath = path.join("/tmp", fieldname);
+
+    console.log(FilePath, fieldname);
+
+    fs.mkdir(FilePath, { recursive: true }, (err) => {
+      if (err) {
+        return cb(err, null)
+      }
+    });
+    cb(null, FilePath)
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, uniqueSuffix + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
