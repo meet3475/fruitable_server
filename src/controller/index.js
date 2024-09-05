79b31cda14b24361979e79c49f65ecee45882180
meet3475 <meetdobariya480@gmail.com>
2024-09-05 10:57:11 +0530

    done_api

--- src/controller/index.js
+++ src/controller/index.js
@@ -9,3 +9,11 @@ module.exports.variantsController = require("./variants.controller");
 module.exports.salespeopleController = require("./salespeople.controller");
 
 module.exports.usersController = require("./users.controller");
+
+module.exports.reviewsController = require("./reviews.controller");
+
+module.exports.ordersController = require("./orders.controller");
+
+module.exports.paymentsController = require("./payments.controller");
+
+module.exports.cartsController = require("./carts.controller");
